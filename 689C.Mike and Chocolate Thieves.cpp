1c143db06a01cb951a02bb5d1cb3ffdad81da472
Shuangquan Feng <fengsq@utexas.edu>
2018-03-28 16:57:07 -0500

    689C

---
+++ 689C.Mike and Chocolate Thieves.cpp
@@ -0,0 +1,94 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <numeric>
+#include <memory>
+using namespace std;
+
+#define PB(x)               push_back(x)
+#define F                   first
+#define S                   second
+
+#define REP(i,from,to)      for(int i=(from); i<=(to); ++i)
+#define PER(i,from,to)      for(int i=(from); i>=(to); --i)
+
+#define FOR(i,less_than)    for (int i=0; i<(less_than); ++i)
+#define FORI(i, container)  for (int i=0; i<(container).size(); ++i)
+#define ROFI(i, container)  for(int i=(container).size()-1; i>=0; --i)
+
+#define FOREACH(elem, container)  for (auto elem : (container))
+#define FILL(container, value)    memset(container, value, sizeof(container))
+#define ALL(container)            (container).begin(), (container).end()
+#define SZ(container)             (int)((container).size())
+
+#define BACK(set_map)       *prev((set_map).end(), 1)
+#define FRONT(set_map)      *(set_map).begin()
+
+inline void _RD(int &x) { scanf("%d", &x); }
+inline void _RD(long long &x) { scanf("%lld", &x); }
+inline void _RD(double &x) { scanf("%lf", &x); }
+inline void _RD(long double &x) { scanf("%Lf", &x); }
+inline void _RD(char &x) { scanf(" %c", &x); }
+inline void RD() {}
+template<class T, class... U> 
+inline void RD(T &head, U &... tail) { _RD(head); RD(tail...); }
+
+using PII = pair<int,int>;
+using LL  = long long;
+using VI  = vector<int>;
+using VLL = vector<LL>;
+using VVI = vector<VI>;
+
+LL count(LL n) {
+    LL ans = 0;
+    for (LL k=2; k*k*k<=n; ++k) 
+        ans += n/(k*k*k);
+    return ans;
+}
+
+int main() {
+    LL m;
+    RD(m);
+
+    LL l = 1, r = 1e16;
+
+    while (l < r) {
+        LL mid = l + ((r-l)>>1);
+        LL cnt = count(mid);
+        if (cnt>=m) 
+            r = mid;
+        else 
+            l = mid + 1;
+    }
+
+    if (count(l)==m)
+#ifdef ONLINE_JUDGE
+        printf("%I64d\n", l);
+#else
+        printf("%lld\n", l);
+#endif
+    else 
+        printf("-1\n");
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
