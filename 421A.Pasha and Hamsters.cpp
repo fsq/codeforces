ff186e0543f73bc55057dca81f165ebb0f5dce35
Shuangquan Feng <fengsq@utexas.edu>
2018-04-03 16:03:06 -0500

    421A

---
+++ 421A.Pasha and Hamsters.cpp
@@ -0,0 +1,72 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <numeric>
+#include <memory>
+using namespace std;
+
+#define PB                  push_back
+#define F                   first
+#define S                   second
+
+#define REP(i,from,to)      for(auto i=(from); i<=(to); ++i)
+#define PER(i,from,to)      for(auto i=(from); i>=(to); --i)
+#define REP_IF(i,from,to,assert)   for(auto i=(from); i<=(to); ++i) if (assert)
+
+#define FOR(i,less_than)    for (auto i=0; i<(less_than); ++i)
+#define FORI(i, container)  for (auto i=0; i<(container).size(); ++i)
+#define FORI_IF(i, container, assert) for (auto i=0; i<(container).size(); ++i) if (assert)
+#define ROFI(i, container)  for (auto i=(container).size()-1; i>=0; --i)
+
+#define FOREACH(elem, container)  for (auto elem : (container))
+#define FILL(container, value)    memset(container, value, sizeof(container))
+#define ALL(container)            (container).begin(), (container).end()
+#define SZ(container)             (int)((container).size())
+
+#define BACK(set_map)       *prev((set_map).end(), 1)
+#define FRONT(set_map)      *(set_map).begin()
+
+inline void _RD(int &x) { scanf("%d", &x); }
+inline void _RD(long long &x) { scanf("%lld", &x); }
+inline void _RD(double &x) { scanf("%lf", &x); }
+inline void _RD(long double &x) { scanf("%Lf", &x); }
+inline void _RD(char &x) { scanf(" %c", &x); }
+inline void RD() {}
+template<class T, class... U> 
+inline void RD(T &head, U &... tail) { _RD(head); RD(tail...); }
+
+using PII = pair<int,int>;
+using LL  = long long;
+using VI  = vector<int>;
+using VLL = vector<LL>;
+using VVI = vector<VI>;
+
+int main() {
+
+    int n, a, b;
+    RD(n, a, b);
+
+    VI ans(n+1, 2);
+    int x;
+    REP(i, 1, a) {
+        RD(x);
+        ans[x] = 1;
+    }
+
+    REP(i, 1, n) 
+        printf("%d ", ans[i]);
+    printf("\n");
+
+    return 0;
+}
