a7773a8e0a3ad63e384a0e303ccc73750a3a29af
Shuangquan Feng <fengsq@utexas.edu>
2018-04-15 21:35:32 -0500

    lg1373

--- lg1373.cpp
+++ lg1373.cpp
@@ -6,34 +6,39 @@ using namespace std;
 int n, m, k;
 const int NN = 803;
 const int MD = 1e9 + 7;
-int a[NN][NN], f[NN][NN][63];
+int a[NN][NN], f[NN][NN][20];
 
-inline void add(int &x, int y) {
-    x += y;
-    if (x>=MD) x-=MD;
-}
+inline void add(int &x, int y) { (x+=y) %= MD; }
 
-void trans(int i, int j, int x, int y, int w, int v) {
-    REP(c, -(k<<1), (k<<1)) 
-        if (f[i][j][c+(k<<1)]) {
-            int nw = c + w - v;
-        }
+void trans(int i, int j, int x, int y, int w) {
+    int nw = (w-a[x][y]+k) % k;
+    REP(c, 0, k-1) 
+        if (f[i][j][c]) 
+            add(f[x][y][(c+nw+k) % k], f[i][j][c]);
 }
 
 int main() {
-    scanf("%d %d %d", &n, &m, &k);
+    scanf("%d %d %d", &n, &m, &k);++k;
     REP(i, 1, n)
         REP(j, 1, m) scanf("%d", &a[i][j]);
-
+    int ans = 0;
     REP(i, 1, n)
-        REP(j, 1, m) {  
-            if (i-2>0) trans(i-2, j, i, j, a[i-1][j], a[i][j]);
-            if (j-2>0) trans(i, j-2, i, j, a[i][j-1], a[i][j]);
-            if (i-1>0 && j-1>0) {
-                trans(i-1, j-1, i, j, a[i-1][j], a[i][j]);
-                trans(i-1, j-1, i, j, a[i][j-1], a[i][j]);
+        REP(j, 1, m) {   
+            add(ans, f[i][j][0]);
+            if (j+1<=m) 
+                // add(f[i][j+1][(a[i][j]-a[i][j+1]+k) % k], 1);
+                ++f[i][j+1][(a[i][j]-a[i][j+1]+k) % k];
+            if (i+1<=n) 
+                // add(f[i+1][j][(a[i][j]-a[i+1][j]+k) % k], 1);
+                ++f[i+1][j][(a[i][j]-a[i+1][j]+k) % k];
+            if (i+2<=n) trans(i, j, i+2, j, a[i+1][j]);
+            if (j+2<=m) trans(i, j, i, j+2, a[i][j+1]);
+            if (i+1<=n && j+1<=m) {
+                trans(i, j, i+1, j+1, a[i+1][j]);
+                trans(i, j, i+1, j+1, a[i][j+1]);
             }
-            add(f[i][j][0], 1);
         }
+
+    printf("%d\n", ans);
     return 0;
 }
