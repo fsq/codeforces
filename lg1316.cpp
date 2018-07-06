1f4d2c1fce2cfb5f4e76b1cf56f6e9eb201eb166
Shuangquan Feng <fengsq@utexas.edu>
2018-07-05 20:02:58 -0500

    lg1316

---
+++ lg1316.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> a;
+
+int partition(const vector<int>& a, int dis) {
+    int cnt = 0;
+    for (int j,i=0; i<a.size(); ) {
+        ++cnt;
+        for (j=i+1; j<a.size() && a[j]-a[i]<dis; ++j);
+        i = j;
+    }
+    return cnt;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin >> n >> m;
+    a.resize(n);
+    for (int i=0; i<n; ++i) cin >> a[i];
+    sort(a.begin(), a.end());
+
+    int l = 1, r = a.back()-a.front();
+    while (l<r) {
+        int mid = l + ((r-l)>>1);
+        if (partition(a, mid) < m)
+            r = mid - 1;
+        else {
+            l = mid;
+            if (r == l+1) 
+                if (partition(a, r)>=m) 
+                    l = r;
+                else
+                    r = l;
+            else {}
+        }
+    }
+
+    cout << l << endl;
+
+    return 0;
+}
