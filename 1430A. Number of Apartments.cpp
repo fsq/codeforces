163727329b18ed47544c6deeae3cbe5838293e5c
blackhold02 <sq_feng@foxmail.com>
2020-10-16 09:54:54 -0700

    easy

---
+++ 1430A. Number of Apartments.cpp
@@ -0,0 +1,91 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <numeric>
+#include <memory>
+#include <list>
+#include <climits>
+#include <fstream>
+#include <sstream>
+#include <random>
+#include <functional>
+
+#define PB                  push_back
+#define F                   first
+#define S                   second
+
+#define REP(i,from,to)      for(auto i=(from); i<=(to); ++i)
+#define PER(i,from,to)      for(auto i=(from); i>=(to); --i)
+#define REP_IF(i,from,to,assert)   for(auto i=(from); i<=(to); ++i) if (assert)
+
+#define FOR(i,less_than)    for (auto i=0; i<(less_than); ++i)
+#define FORI(i, container)  for (auto i=0; i<(container).size(); ++i)
+#define FORI_IF(i, container, assert) for (auto i=0; i<(container).size(); ++i) if (assert)
+#define ROFI(i, container)  for (auto i=SZ(container)-1; i>=0; --i)
+#define FOREACH(elem, container)  for (auto elem : (container))
+#define FOREACH_IF(elem, container, assert) for (auto elem : (container)) if (assert)
+
+#define MEMSET(container, value)  memset(container, value, sizeof(container))
+#define MEMSET0(container)        memset(container, 0, sizeof(container))
+#define FILL(container, value)    fill(container.begin(), container.end(), value)
+#define FILL0(container)    fill(container.begin(), container.end(), 0)
+#define ALL(container)            (container).begin(), (container).end()
+#define SZ(container)             (int)((container).size())
+
+#define BACK(set_map)       *prev((set_map).end(), 1)
+#define FRONT(set_map)      *(set_map).begin()
+
+#define POP(var, container) auto var=(container.front()); container.pop();
+
+using PII = std::pair<int,int>;
+using LL  = long long;
+using VI  = std::vector<int>;
+using CVI = const VI;
+using VLL = std::vector<LL>;
+using VVI = std::vector<VI>;
+using VVLL = std::vector<VLL>;
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+
+  VI f(1001);
+  f[0] = true;
+  REP(i, 1, 1000)
+    f[i] = (i>=3 && f[i-3]) || (i>=5 && f[i-5]) || (i>=7 && f[i-7]);
+
+  VI stp = {3, 5, 7};
+  int t, n;
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    if (!f[n]) {
+      cout << "-1\n";
+      continue;
+    }
+    VI ans(3);
+    while (n)
+      FOR(i, 3)
+        if (n>=stp[i] && f[n-stp[i]]) {
+          ++ans[i];
+          n -= stp[i];
+          break;
+        }
+    FOREACH(x, ans) cout << x << ' ';
+    cout << endl;
+  }
+
+}
